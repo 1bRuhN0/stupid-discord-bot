b575ef79523fe515ab767c7bc63d8c9db70e145a
1bRuhN0 <95714959+1bRuhN0@users.noreply.github.com>
2022-11-11 13:02:49 -0300

    index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+const { CommandClient } = require('eris')
+
+// Stupid ass bot creation
+async function init(token) {
+    const stupidAssBot = new CommandClient(`Bot ${token}`, { intents: ['guilds'], maxShards: 'auto',restMode: true })
+    // Register the stupid ass command
+    stupidAssBot.on('ready', async () => {
+        await stupidAssBot.bulkEditCommands([{
+            name: 'bruno',
+            description: 'discord time',
+            type: 1,
+        }])
+        console.log(`Paste the URL below into your browser to invite your bot!\nhttps://discord.com/oauth2/authorize?client_id=${stupidAssBot.user.id}&scope=applications.commands%20bot&permissions=3072`)
+    })
+    // Stupid ass interaction creation event
+    stupidAssBot.on('interactionCreate', async (interaction) => {
+        if (interaction?.data?.name === 'lol') {
+            await interaction.createMessage({
+                content: 'discord i'm howling at the moon and sleeping in the middle of a summer afternoon'
+            })
+            console.log('Self destructing...')
+            process.exit(0)
+        }
+    })
+    stupidAssBot.connect();
+}
+
+const tokenFromStupidCommand = process.argv[2]
+init(tokenFromStupidCommand);
